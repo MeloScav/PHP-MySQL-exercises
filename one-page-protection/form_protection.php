81efbe89433c50f818b2b1e09d666ea0649ad746
MeloScav <meloscavezzoni@gmail.com>
2021-01-02 16:43:59 +0100

    Form display

--- one-page-protection/form_protection.php
+++ one-page-protection/form_protection.php
@@ -6,6 +6,23 @@
     <title>Page protégée</title>
 </head>
 <body>
-    
+   <?php
+        // If the password is null or false
+        if(!isset($_POST['password']) OR $_POST['password'] != 'kangourou'){
+   ?> 
+
+    <h1>Entrez le mot de passe pour vous connectez et accéder aux codes secrets</h1>
+
+    <form action="form_protection.php" method="post">
+        <div>
+            <label for="password">Entrez le mot de passe</label>
+            <input type="password" name="password" id="password">
+        </div>
+        <input type="submit" value="Valider">
+    </form>
+
+   <?php
+        }
+   ?>
 </body>
 </html>
