be9e1bbda14bf3fda04210abed42d152054df03c
MeloScav <meloscavezzoni@gmail.com>
2021-01-02 14:08:22 +0100

    Basic structure + title

---
+++ password-protected-page/form_page.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Formulaire</title>
+</head>
+<body>
+    <h1>Entrez le mot de passe pour vous connectez et accéder aux codes secrets</h1>
+    
+</body>
+</html>
