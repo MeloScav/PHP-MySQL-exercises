9528582daec155e506a921d3bae50d7130666780
MeloScav <meloscavezzoni@gmail.com>
2021-01-02 14:37:17 +0100

    Chech value and show page

--- password-protected-page/confidential_page.php
+++ password-protected-page/confidential_page.php
@@ -6,6 +6,17 @@
     <title>Page confidentielle</title>
 </head>
 <body>
+    <?php
+    // Check if the value exists and if correct
+        if(isset($_POST['password']) AND $_POST['password'] == "kangourou") {
+            // If true: show page
+    ?>
     
+    <h1>Voici les codes secret</h1>
+    <p><strong>H5TM-TW35-Y5E6-W58P-53FG-I8O5</strong></p>
+
+    <?php
+     } 
+    ?>
 </body>
 </html>
