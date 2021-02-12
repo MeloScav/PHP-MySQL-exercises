ef421e2671d2a9c7a13b3faa14e66c39f711d75d
Méloé <meloscavezzoni@gmail.com>
2021-02-12 07:37:45 +0100

    Word correction

--- little-chat-room/littlechat_post.php
+++ little-chat-room/littlechat_post.php
@@ -1,6 +1,6 @@
 <?php
 
-    // BDD connexion
+    // BDD connection
     try {
         $bdd = new PDO('mysql:host=localhost;dbname=test;charset=utf8', 'root', '',
                         array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
