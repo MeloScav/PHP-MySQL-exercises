5250a7e2640f4a4c71bbd69243fe567f9e463803
Méloé <meloscavezzoni@gmail.com>
2021-02-17 21:54:55 +0100

    Database connection

--- blog/database.php
+++ blog/database.php
@@ -3,7 +3,7 @@
     require('./../config.php');
     // BDD connection
     try {
-        $bdd = new PDO($DB_CONNECTION,  $DB_USERNAME, $DB_PASSWORD,
+        $db = new PDO($DB_CONNECTION,  $DB_USERNAME, $DB_PASSWORD,
                         array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
     }
     catch (Exception $e) {
