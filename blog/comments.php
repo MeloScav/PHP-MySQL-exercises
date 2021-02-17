f1f72958506000bbdab306475cb08beea4182770
Méloé <meloscavezzoni@gmail.com>
2021-02-17 20:26:21 +0100

    Basic structure

---
+++ blog/comments.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Commentaires</title>
+</head>
+<body>
+    <h1>Commentaires</h1>
+    <button type="button">Retour</button>
+</body>
+</html>
---
+++ blog/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Les news</title>
+</head>
+<body>
+
+    <h1>Bienvenue sur mon blog !</h1>
+    <h2>Voici les derniers nouvelles du blog :</h2>
+    
+</body>
+</html>
