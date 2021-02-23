6a2c5f884f2956c1b7fca28f39f9507bc586ca0e
Méloé <meloscavezzoni@gmail.com>
2021-02-23 17:07:11 +0100

    Db + request + content structure

--- blog/comments.php
+++ blog/comments.php
@@ -8,6 +8,34 @@
 </head>
 <body>
     <h1>Commentaires</h1>
-    <button type="button">Retour</button>
+    <a>Retour</a>
+
+    <?php
+        // Connection to the database
+        require("./database.php");
+
+        $id_news= $_GET['news'];
+    
+        $request = $db->prepare('SELECT new_id, author, comment, DATE_FORMAT(comment_date, "%d/%m/%Y %H:%i") AS comment_date_new FROM comments GROUP BY new_id HAVING new_id = :url_id');
+
+        $request->execute(array(':url_id' => $id_news));
+
+        while($data = $request->fetch()) {
+    ?>
+        <div class="container">
+            <div class="header-comments">
+                <h3><?php echo $data['author'] ?></h3>
+                <p><?php echo $data['comment_date_new'] ?></p>
+            </div>
+            <div class="content-comment">
+                <p><?php echo $data['comment'] ?></p>
+            </div>
+        </div>
+    <?php
+        }
+
+        $request->closeCursor();
+        $db = null;
+    ?>
 </body>
 </html>
