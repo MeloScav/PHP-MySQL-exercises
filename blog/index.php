03b01adc82a2e63e91e60e26392c33f38c3d6516
Méloé <meloscavezzoni@gmail.com>
2021-02-25 13:21:31 +0100

    5 news per page + paging

--- blog/index.php
+++ blog/index.php
@@ -18,9 +18,35 @@
         // Connection to the database
         require("./database.php");
 
-        $response = $db->query('SELECT ID, title, content, DATE_FORMAT(creation_date, "le %d/%m/%Y à %H:%i") AS date_news FROM news');
+         // Which page
+        if (isset($_GET['page']) && !empty($_GET['page'])) {
+            $current_page = strip_tags($_GET['page']);
+        } else {
+            $current_page = 1;
+        }
 
-        while ($data = $response->fetch()) {
+        // Count the number of News
+        $nb_of_news = $db->query('SELECT COUNT(*) AS nb_news FROM news');
+        // Get the number
+        $total_news = $nb_of_news->fetch();
+        $total_nb_news = $total_news['nb_news'];
+
+        // 5 news per pages
+        $per_page = 5;
+        // Number of page
+        $pages = ceil($total_nb_news / $per_page);
+
+        // Calculation of the first message of the page
+        $first_news = ($current_page * $per_page) - $per_page;
+
+        $request = $db->prepare('SELECT ID, title, content, DATE_FORMAT(creation_date, "le %d/%m/%Y à %H:%i") AS date_news FROM news ORDER BY ID DESC LIMIT :first,:perpage');
+
+        $request->bindValue(':first', $first_news, PDO::PARAM_INT);
+        $request->bindValue(':perpage', $per_page, PDO::PARAM_INT);
+
+        $request->execute();
+
+        while ($data = $request->fetch()) {
         ?>
 
             <div class="container-news">
@@ -38,7 +64,37 @@
 
         <?php
         }
-        $response->closeCursor();
+        ?>
+
+        <!-- Paging -->
+        <nav>
+            <ul class="paging">
+                <!-- Deactivate the previous page if you are on the 1st page -->
+                <li class="page-item <?php echo ($current_page == 1) ? "disabled" : ""; ?>">
+                    <a href="?page=<?php echo $current_page-1; ?>" class="link">
+                        Page précédente
+                    </a>
+                </li>
+                <!-- Links to pages -->
+                <?php for($page = 1; $page <= $pages; $page++) : ?>
+                    <li class="page-item <?php echo ($current_page == $page) ? "active" : ""; ?>">
+                        <a href="?page=<?php echo $page ?>" class="page-link">
+                            <?php echo $page ?>
+                        </a>
+                    </li>
+                <?php endfor ?>
+                <!-- Link to the next page disabled if you are on the last page  -->
+                <li class="page-item <?php echo ($current_page == $pages) ? "disabled" : "" ?>">
+                    <a href="?page=<?php echo $current_page+1 ?>" class="page-link">
+                        Page suivante
+                    </a>
+                </li>
+            </ul>
+        </nav>
+
+
+        <?php
+        $request->closeCursor();
         $db = null;
         ?>
     </div>
