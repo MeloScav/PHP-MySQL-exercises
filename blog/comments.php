3262f649bbbb231df8fd2c642df65c97a0eb86da
Méloé <meloscavezzoni@gmail.com>
2021-02-25 10:41:39 +0100

    Link CSS and the container class added

--- blog/comments.php
+++ blog/comments.php
@@ -4,54 +4,57 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
     <title>Commentaires</title>
 </head>
 <body>
     <h1>Commentaires</h1>
-    <a href="index.php">Retour</a>
+    <a class="btn" href="index.php">Retour</a>
 
-    <?php
-        // Connection to the database
-        require("./database.php");
+    <div class="container">
+        <?php
+            // Connection to the database
+            require("./database.php");
 
-        $id_news= $_GET['news'];
+            $id_news= $_GET['news'];
 
-        $request_news = $db->prepare('SELECT ID, title, content, DATE_FORMAT(creation_date, "%d/%m/%Y %H:%i") AS date_news FROM news WHERE ID = :id_url');
-        $request_news->execute(array(':id_url' => $id_news));
+            $request_news = $db->prepare('SELECT ID, title, content, DATE_FORMAT(creation_date, "%d/%m/%Y %H:%i") AS date_news FROM news WHERE ID = :id_url');
+            $request_news->execute(array(':id_url' => $id_news));
 
-        while($data_news = $request_news->fetch()) {
-    ?>
-        <div class="container">
-            <div class="header-news">
-                <h3><?php echo $data_news['title'] ?></h3>
-                <p><?php echo $data_news['date_news'] ?></p>
+            while($data_news = $request_news->fetch()) {
+        ?>
+            <div class="container-news">
+                <div class="header-news">
+                    <h3><?php echo $data_news['title'] ?></h3>
+                    <p><?php echo $data_news['date_news'] ?></p>
+                </div>
+                <div class="content-news">
+                    <p><?php echo $data_news['content'] ?></p>
+                </div>
             </div>
-            <div class="content-news">
-                <p><?php echo $data_news['content'] ?></p>
-            </div>
-        </div>
-    <?php
-        }
-    
-        $request_comments = $db->prepare('SELECT new_id, author, comment, DATE_FORMAT(comment_date, "%d/%m/%Y %H:%i") AS comment_date_new FROM comments GROUP BY new_id HAVING new_id = :url_id');
-        $request_comments->execute(array(':url_id' => $id_news));
+        <?php
+            }
+        
+            $request_comments = $db->prepare('SELECT new_id, author, comment, DATE_FORMAT(comment_date, "%d/%m/%Y %H:%i") AS comment_date_new FROM comments GROUP BY new_id HAVING new_id = :url_id');
+            $request_comments->execute(array(':url_id' => $id_news));
 
-        while($data = $request_comments->fetch()) {
-    ?>
-        <div class="container">
-            <div class="header-comments">
-                <h3><?php echo $data['author'] ?></h3>
-                <p><?php echo $data['comment_date_new'] ?></p>
+            while($data = $request_comments->fetch()) {
+        ?>
+            <div class="container-comments">
+                <div class="header-comments">
+                    <h3><?php echo $data['author'] ?></h3>
+                    <p><?php echo $data['comment_date_new'] ?></p>
+                </div>
+                <div class="content-comment">
+                    <p><?php echo $data['comment'] ?></p>
+                </div>
             </div>
-            <div class="content-comment">
-                <p><?php echo $data['comment'] ?></p>
-            </div>
-        </div>
-    <?php
-        }
-        $request_news->closeCursor();
-        $request_comments->closeCursor();
-        $db = null;
-    ?>
+        <?php
+            }
+            $request_news->closeCursor();
+            $request_comments->closeCursor();
+            $db = null;
+        ?>
+    </div>
 </body>
 </html>
--- blog/index.php
+++ blog/index.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./style.css">
     <title>Les news</title>
 </head>
 <body>
@@ -11,34 +12,36 @@
     <h1>Bienvenue sur mon blog !</h1>
     <h2>Voici les derniers nouvelles du blog :</h2>
 
-    <?php
-    
-    // Connection to the database
-    require("./database.php");
+    <div class="container">
+        <?php
+        
+        // Connection to the database
+        require("./database.php");
 
-    $response = $db->query('SELECT ID, title, content, DATE_FORMAT(creation_date, "%d/%m/%Y %H:%i") AS date_news FROM news');
+        $response = $db->query('SELECT ID, title, content, DATE_FORMAT(creation_date, "%d/%m/%Y %H:%i") AS date_news FROM news');
 
-    while ($data = $response->fetch()) {
-    ?>
+        while ($data = $response->fetch()) {
+        ?>
 
-        <div class="container">
-            <div class="header-news">
-                <h3><?php echo $data['title'] ?></h3>
-                <p><?php echo $data['date_news'] ?></p>
+            <div class="container-news">
+                <div class="header-news">
+                    <h3><?php echo $data['title'] ?></h3>
+                    <p><?php echo $data['date_news'] ?></p>
+                </div>
+                <div class="content-news">
+                    <p><?php echo $data['content'] ?></p>
+                </div>
+                <a href="comments.php?news=<?php echo $data['ID']?>">
+                    Commentaires
+                </a>
             </div>
-            <div class="content-news">
-                <p><?php echo $data['content'] ?></p>
-            </div>
-            <a href="comments.php?news=<?php echo $data['ID']?>">
-                Commentaires
-            </a>
-        </div>
 
-    <?php
-    }
-    $response->closeCursor();
-    $db = null;
-    ?>
+        <?php
+        }
+        $response->closeCursor();
+        $db = null;
+        ?>
+    </div>
     
 </body>
 </html>
