aa8b53297af1153137076154fbfd829649248c0d
Méloé <meloscavezzoni@gmail.com>
2021-03-03 11:59:52 +0100

    Stylized form

--- blog/comments.php
+++ blog/comments.php
@@ -37,7 +37,8 @@
             $request_news->closeCursor();
         ?>
 
-        <h2>Commentaires</h2>
+        <div class="container-comments">
+            <h2>Commentaires</h2>
         
         <?php
         
@@ -60,23 +61,27 @@
             $request_comments->closeCursor();
             $db = null;
         ?>
-
-         <!-- FORM -->
-        <h2>Écrire un commentaire :</h2>
-        <form id="formulaire" class="comment-form" action="comments_post.php?news=<?php echo $id_news ?>" method="post">
-            <div class="form-input">
-                <label for="name">Nom :</label>
-                <label for="message">Message :</label>
+        </div>
+    </div>
+    <!-- FORM -->
+    <div class="container-form-background">
+        <div class="container">
+            <h2>Écrire un commentaire :</h2>
+            <form id="formulaire" class="comment-form" action="comments_post.php?news=<?php echo $id_news ?>" method="post">
+                <div class="form-input">
+                    <label for="name">Nom :</label>
+                    <label for="message">Message :</label>
+                </div>
+                <div class="form-input">
+                    <input type="text" name="name" id="name" required>
+                    <textarea name="message" id="message" cols="30" rows="5" required></textarea>
+                </div>
+            </form>
+            <div class="buttons">
+                <input class="btn-form" type="submit" form="formulaire" value="Envoyer">
+                <button class="btn-form" type="button">Actualiser</button>
             </div>
-            <div class="form-input">
-                <input type="text" name="name" id="name" required>
-                <textarea name="message" id="message" cols="30" rows="5" required></textarea>
-            </div>
-        </form>
-            <div>
-                <input type="submit" form="formulaire" value="Envoyer">
-                <button type="button">Actualiser</button>
-            </div>
+        </div>
     </div>
 
 </body>
