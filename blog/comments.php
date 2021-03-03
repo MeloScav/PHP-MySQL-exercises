73bd6ee7b34c9e4ed760c032b010d4e837844761
Méloé <meloscavezzoni@gmail.com>
2021-03-03 15:53:15 +0100

    Include: form

--- blog/comments.php
+++ blog/comments.php
@@ -63,26 +63,8 @@
         ?>
         </div>
     </div>
-    <!-- FORM -->
-    <div class="container-form-background">
-        <div class="container">
-            <h2>Écrire un commentaire :</h2>
-            <form id="formulaire" class="comment-form" action="comments_post.php?news=<?php echo $id_news ?>" method="post">
-                <div class="form-input">
-                    <label for="name">Nom :</label>
-                    <label for="message">Message :</label>
-                </div>
-                <div class="form-input">
-                    <input type="text" name="name" id="name" required>
-                    <textarea name="message" id="message" cols="30" rows="5" required></textarea>
-                </div>
-            </form>
-            <div class="buttons">
-                <input class="btn-form" type="submit" form="formulaire" value="Envoyer">
-                <button class="btn-form" type="button">Actualiser</button>
-            </div>
-        </div>
-    </div>
+   
+    <?php include('./form.php'); ?>
 
 </body>
 </html>
